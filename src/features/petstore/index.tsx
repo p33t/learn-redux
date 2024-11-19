88b4cf0abb7667251d5bea0cd475c6a5d93382d1
pwl <pwl@localhost>
2024-11-19 10:27:29 -0800

    [API] Selectable petstore query param

--- src/features/petstore/index.tsx
+++ src/features/petstore/index.tsx
@@ -2,12 +2,16 @@ import { useState } from "react"
 import * as petApi from "../../store/petApi"
 import styles from "../quotes/Quotes.module.css"
 
+import type { Pet } from "../../store/petApi"
+
+type PetStatus =  Pet['status']
+type PetStatusArg = PetStatus | undefined
 
 export const Ui = () => {
-  const [numberOfQuotes, setNumberOfQuotes] = useState(10)
+  const [petStatus, setPetStatus] = useState<PetStatusArg>("available")
   // Using a query hook automatically fetches data and returns query values
   const { data, isError, isLoading, isSuccess } =
-    petApi.useFindPetsByStatusQuery({status: 'available'})
+    petApi.useFindPetsByStatusQuery({status: petStatus})
 
   if (isError) {
     return (
@@ -31,12 +35,13 @@ export const Ui = () => {
         <h3>Select the status:</h3>
         <select
           className={styles.select}
-          value={numberOfQuotes}
+          value={petStatus}
           onChange={e => {
-            setNumberOfQuotes(Number(e.target.value))
+            setPetStatus(e.target.value as PetStatusArg)
           }}
         >
-          {[5, 10, 20].map(option => (
+          {/*Need to hard-code options because cannot iterate string union type*/}
+          {["available", "pending", "sold"].map(option => (
             <option key={option} value={option}>
               {option}
             </option>
